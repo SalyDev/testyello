c9ef1df44423e2af9d814adc1d05ad711594421a
Saly <saly@applize.tech>
2024-10-08 11:06:19 +0000

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { CoursListComponent } from './cours-list/cours-list.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CourseDetailsComponent,
+    CoursListComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent
   ],
   imports: [
     BrowserModule,
--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,3 +1,4 @@
+import { style } from "@angular/animations";
 import { categories, CourseCategory } from "./categories";
 
 export interface Course {
@@ -6,53 +7,61 @@ export interface Course {
     rate: number;
     title: string;
     duration: string;
+    style: string;
   }
   
   export const courses = [
     {
       id: 1,
-      category: categories[2],
+      category: categories[1],
       rate: "4.5",
       title: "Les fonctions trigonométriques",
-      duraton: "30 min"
+      duraton: "30 min",
+      style: "bg-green"
     },
 
     {
         id: 1,
-        category: categories[1],
-        rate: "4.5",
-        title: "Les fonctions trigonométriques",
-        duraton: "2h 15min"
+        category: categories[2],
+        rate: "5",
+        title: "Les base de la dynamique",
+        duraton: "2h 15min",
+        style: "bg-red",
       },
       {
         id: 1,
-        category: categories[2],
-        rate: "4.5",
-        title: "Les fonctions trigonométriques",
-        duraton: "30 min"
+        category: categories[3],
+        rate: "4.7",
+        title: "L'activité cardiaque",
+        duraton: "1h 30min",
+        style: "bg-orange",
       },
   
       {
           id: 1,
-          category: categories[1],
-          rate: "4.5",
-          title: "Les fonctions trigonométriques",
-          duraton: "2h 15min"
+          category: categories[4],
+          rate: "3.5",
+          title: "Les courants littéraires",
+          duraton: "2h",
+          style: "bg-purple",
         },
         {
             id: 1,
             category: categories[2],
-            rate: "4.5",
-            title: "Les fonctions trigonométriques",
-            duraton: "30 min"
+            rate: "5",
+            title: "Gravitation universelle",
+            duraton: "20 min",
+            style: "bg-red",
           },
+          
       
           {
               id: 1,
-              category: categories[1],
-              rate: "4.5",
-              title: "Les fonctions trigonométriques",
-              duraton: "2h 15min"
+              category: categories[5],
+              rate: "4",
+              title: "La seconde guerre mondiale",
+              duraton: "1h 20min",
+              style: "bg-green",
             },
     
   ];
